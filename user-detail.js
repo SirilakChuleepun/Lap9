a0145bc8ea908648bcc6b3c37689555d519464e9
66160157 <66160157@go.buu.ac.th>
2025-02-14 15:51:14 +0700

    Add code in user-detail.js is funtion fetchUserDetail

---
+++ user-detail.js
@@ -0,0 +1,25 @@
+const userDetail = document.getElementById("user-detail");
+const viewPostsBtn = document.getElementById("veiw-posts");
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get("id");
+
+async function fetchUserDetail() {
+    try{
+        let response = await fetch(`https://jsonplaceholder.typicode.con/user/${userId}`);
+        let user = await response.json();
+        userDetail.innerHTML = `
+            <h2>${user.name}</h2>
+            <p><strong>Email:</strong>${user.email}</p>
+            <p><strong>Phone:</strong>${user.phone}</p>
+            <p><strong>Website:</strong>${user.website}</p>
+            <p><strong>Company:</strong>${user.company.name}</p>
+        `;
+        viewPostsBtn.onclick = () => window.location.href = `user-posts.html?id=${user.id}`;
+    } catch (error){
+        console.error("Error fetching user details:", error);
+        userDetail.innerHTML = "<p>โหลดข้อมูลผิดพลาด</p>";
+    }
+    
+}
+
+fetchUserDetail();
