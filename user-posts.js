83e10e4d6227b031393ff29b3bb569b380f2b1df
66160157 <66160157@go.buu.ac.th>
2025-02-14 16:20:58 +0700

    Add code in user-posts.js is funtion fetchUserposts

---
+++ user-posts.js
@@ -0,0 +1,50 @@
+const postsList = document.getElementById("posts-list");
+const userName = document.getElementById("user-name");
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get("id");
+
+async function fetchUserPosts() {
+    try {
+        let userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        let user = await userResponse.json();
+        userName.textContent = user.name;
+
+        let postsResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`);
+        let posts = await postsResponse.json();
+        postsList.innerHTML = posts.map(post => `
+            <div class="post">
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <button class="comment-btn" data-post-id="${post.id}">ดูความคิดเห็น</button>
+                <div class="comments" id="comments-${post.id}" style="display: none;"></div>
+            </div>
+        `).join("");
+
+        document.querySelectorAll(".comment-btn").forEach(button =>{
+            button.addEventListener("click", async (event) =>{
+                let postId = event.target.getAttribute("data-post-id");
+                let commentsDiv = document.getElementById(`comments-${postId}`);
+                if (commentsDiv.style.display == "none") {
+                    let commentsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+                    let comments = await commentsResponse.json();
+                    commentsDiv.innerHTML = comments.map(comment => `
+                            <p><strong>${comment.name}</strong>: ${comment.body}</p>
+                        `).join("");
+                        commentsDiv.style.display = "block";
+                        event.target.textContent = "ซ่อนความคิดเห็น";
+                } else {
+                    commentsDiv.style.display = "none";
+                    event.target.textContent = "ดูความคิดเห็น";
+                    
+                }
+            });
+        });
+    } catch (error) {
+        console.error("Error fetching posts:", error);
+        postsList.innerHTML = "<p>โหลดข้อมูลผิดพลาด</p>"ว
+        
+    }
+    
+}
+
+fetchUserPosts();
